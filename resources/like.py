02da49e196d172f11119fd79bce01649ccb4e115
macciachini <macciachini@outlook.com>
2021-05-18 19:22:15 +0200

    change  like

--- resources/like.py
+++ resources/like.py
@@ -38,7 +38,7 @@ class Like(Resource):
 
         if user:
             try:
-                new_like = like_schema.load(request.get_json(), partial=True)
+                new_like = like_schema.load(request.get_json())
             except ValidationError as err:
                 return err.messages, 400
 
